2b38007ecb1b3478aa00335a980c643e8ec25e25
tonni-akter-dev <tanniakhter123@gmail.com>
2023-12-09 16:50:53 +0600

    project upodate

--- src/Authentication/Login/Login.jsx
+++ src/Authentication/Login/Login.jsx
@@ -1,8 +1,32 @@
-
+import loginImage from '../../assets/login.png'
+import './Login.css'
 const Login = () => {
     return (
         <div>
-            
+            <div className="grid grid-cols-2 gap-0">
+                <div>
+                    <img className='w-full h-screen' src={loginImage} alt="login image" />
+                </div>
+                <div>
+                    <div className='flex justify-end p-30'>
+                        <div className='flex items-center gap-30'>
+                            <h4 className='text-lg text_dark_grey '>Already have any account?</h4>
+                            <button className='text-white px-45 py-14 btn_blue_bg '>LogIn</button>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <h1 className='text-5xl font-semibold text-primary'>Email Verification</h1>
+                        <p className='text-base text222color'>Enter the OTP code that was sent your email</p>
+                        <div>
+                            <input type="text" className='input_field' placeholder='dfkjsd' />
+
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,48 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
-      'blue': '#000080',
+      'primary': '#000080',
       'yellow': '#FFA500',
-      'dark_grey': '#222',
+      'text_dark_grey': '#222',
       'light_grey': '#333',
       'sky': '#E8EEFF',
       'dark_sky': '#9AA8D1',
-
+    },
+    padding: {
+      '30': '30px',
+      '14': '14px',
+      '45': '45px'
     },
     fontFamily: {
       sans: ['Work Sans', 'sans-serif'],
     },
+    // fontSize: {
+      
+    // },
+    // fs: {
+    //   '3xl': '48px',
+    //   'xxl': '28px',
+    //   'xl': '24px',
+    //   'lg': '20px',
+    //   'base': '16px',
+    //   'sm': '18px',
+    //   'xm': '14px',
+    // },
     extend: {
-      spacing: {
+      gap: {
         '6': '6px',
         '8': '8px',
         '10': '10px',
         '14': '14px',
         '15': '15px',
         '20': '20px',
+        '30': '30px',
       },
       borderRadius: {
         '3': '3px',
